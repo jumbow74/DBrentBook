edc40d7fbc8f6c59ff2ea57a09d2050f8d5f7041
jumbow74 <ping_jumbow@hotmail.com>
2015-10-26 13:42:36 +0700

    change

--- changePass.php
+++ changePass.php
@@ -8,29 +8,19 @@ session_start();
 Change Password
 <hr>
 <?PHP
+	if (!$connect) {
+			$username = trim($_POST['username']);
 
-	if(isset($_POST['confirmTochange'])){
-		
-		$username = trim($_POST['username']);
-		$Old_password = trim($_POST['Old_password']);
-		$new_pssword = trim($_POST['new_pssword']);
-		$query = "UPDATE AA_LOGIN SET password='$new_pssword' WHERE username='$username' and password='$Old_password' ";
-		$parseRequest = oci_parse($conn, $query);
-		oci_execute($parseRequest);
-		// Fetch each row in an associative array
-		$row = oci_fetch_array($parseRequest, OCI_RETURN_NULLS+OCI_ASSOC);
-		if($row){
-			$_SESSION['ID'] = $row['ID'];
-			$_SESSION['NAME'] = $row['NAME'];
-			$_SESSION['SURNAME'] = $row['SURNAME'];
-			echo '<script>window.location = "MemberPage.php";</script>';
-		}else{
-			echo "changePass fail.";
 		}
-	};
-	//oci_close($conn);
-?>
+		
+			
+		
+		
+	
+	;
 
+?>
+			
 <form action='changePass.php' method='post'>
 	Username <br>
 	<input name='username' type='input'><br>
